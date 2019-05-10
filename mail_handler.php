580b7b463e175354495e6e0526c175dda37a8ffb
East <eastmaels@gmail.com>
2019-05-11 08:01:30 +0900

    add email

--- mail_handler.php
+++ mail_handler.php
@@ -1,7 +1,7 @@
 
 <?php 
 if(isset($_POST['submit'])){
-    $to = "eastmaels@gmail.com"; // this is your Email address
+    $to = "eastmael.uber@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['name'];
     $last_name = $_POST['phone'];
